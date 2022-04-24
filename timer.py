0e380b35a181b40f5fdee904ca1fe000a01a0f0b
Algo04 <Algo04@mail.ru>
2022-04-24 12:14:08 +0500

    v2

---
+++ timer.py
@@ -0,0 +1,4 @@
+from PyQt5.QtCore import Qt, QTimer, QTime 
+from PyQt5.QtCore import QDoubleValidator, QIntValidator, QFont 
+from PyQt5.QtCore import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGridLayout, QGroupBox
+
